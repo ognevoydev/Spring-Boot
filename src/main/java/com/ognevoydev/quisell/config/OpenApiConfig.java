8f3cb99ff91aa34a083db735fda4f38d7fb1cd54
ognevoydev <ognevoy.devmail@gmail.com>
2023-01-09 13:07:38 +0300

    FOO-31: implement swagger

---
+++ src/main/java/com/ognevoydev/quisell/config/OpenApiConfig.java
@@ -0,0 +1,8 @@
+package com.ognevoydev.quisell.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "Quisell API"))
+class OpenAPIConfig {
+}
--- src/main/java/com/ognevoydev/quisell/controller/PostController.java
+++ src/main/java/com/ognevoydev/quisell/controller/PostController.java
@@ -3,37 +3,59 @@ package com.ognevoydev.quisell.controller;
 import com.ognevoydev.quisell.common.exception.HttpStatusException;
 import com.ognevoydev.quisell.model.Post;
 import com.ognevoydev.quisell.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
 import java.security.Principal;
 import java.util.List;
 import java.util.UUID;
+
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/posts")
+@Tag(name = "Объявления")
 public class PostController {
 
     private final PostService postService;
 
     @GetMapping("/{id}")
-    public Post getPostById(@PathVariable(value = "id") UUID postId) {
+    @Operation(summary = "Получить объявление по id")
+    @ApiResponse(responseCode = "200", description = "Запрос выполнен успешно")
+    @ApiResponse(responseCode = "404", description = "Не найдено")
+    public Post getPostById(
+            @Parameter(description = "ID объявления")
+            @PathVariable(value = "id") UUID postId) {
         return postService.getPostById(postId);
     }
 
     @GetMapping
+    @Operation(summary = "Получить список всех активных объявлений")
+    @ApiResponse(responseCode = "200", description = "Запрос выполнен успешно")
     public List<Post> getAllPosts() {
         return postService.getAllPosts();
     }
 
     @PostMapping
+    @Operation(summary = "Создать объявление")
+    @ApiResponse(responseCode = "200", description = "Запрос выполнен успешно")
     public void addPost(@RequestBody Post post) {
         postService.savePost(post);
     }
 
     @DeleteMapping("/{id}")
-    public void deletePost(@PathVariable(value = "id") UUID postId, Principal principal) {
+    @Operation(summary = "Удалить объявление по id")
+    @ApiResponse(responseCode = "200", description = "Запрос выполнен успешно")
+    @ApiResponse(responseCode = "403", description = "Доступ запрещён")
+    @ApiResponse(responseCode = "404", description = "Не найдено")
+    public void deletePost(
+            @Parameter(description = "ID объявления")
+            @PathVariable(value = "id") UUID postId, Principal principal) {
 
         if(postService.isPostOwner(postId, UUID.fromString(String.valueOf(principal)))) {
             postService.deletePostById(postId);
