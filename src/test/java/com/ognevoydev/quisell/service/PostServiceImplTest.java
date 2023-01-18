495487325b1c38aebc2a979f85ea5eb708fccbe9
ognevoydev <ognevoy.devmail@gmail.com>
2023-01-18 20:03:25 +0300

    FOO-29: unit tests for service

--- src/test/java/com/ognevoydev/quisell/service/PostServiceImplTest.java
+++ src/test/java/com/ognevoydev/quisell/service/PostServiceImplTest.java
@@ -1,7 +1,6 @@
 package com.ognevoydev.quisell.service;
 
 import com.ognevoydev.quisell.common.exception.HttpStatusException;
-import com.ognevoydev.quisell.model.dto.PostUpdateDTO;
 import com.ognevoydev.quisell.model.entity.Post;
 import com.ognevoydev.quisell.model.mapper.PostMapper;
 import com.ognevoydev.quisell.repository.PostRepository;
@@ -9,12 +8,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @ActiveProfiles("test")
+@Sql(scripts = "classpath:clean_file.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 class PostServiceImplTest {
 
     @Autowired
@@ -27,7 +29,7 @@ class PostServiceImplTest {
     private PostMapper postMapper;
 
     @Test
-    void deletePostById() {
+    void deletePostCheckThrowsExceptionIfNoPostExist() {
         Post post = new Post();
         post.setTitle("Foo");
         post.setDescription("Foo");
@@ -36,14 +38,23 @@ class PostServiceImplTest {
         postService.savePost(post);
 
         assertThrows(HttpStatusException.class, () -> postService.deletePostById(UUID.randomUUID()));
+    }
 
+    @Test
+    void deletePostCheckIsNotNullIfPostDeleted() {
+        Post post = new Post();
+        post.setTitle("Foo");
+        post.setDescription("Foo");
+        post.setUsed(false);
+
+        postService.savePost(post);
         postService.deletePostById(post.getId());
 
         assertNotNull(postService.getPostById(post.getId()).getDeletedAt());
     }
 
     @Test
-    void isPostOwner() {
+    void postOwnerCheckIsTrueIfOwner() {
         Post post = new Post();
         post.setTitle("Foo");
         post.setDescription("Foo");
@@ -52,27 +63,109 @@ class PostServiceImplTest {
         postService.savePost(post);
 
         assertTrue(postService.isPostOwner(post.getId(), post.getAccountId()));
+    }
+
+    @Test
+    void postOwnerCheckIsFalseIfNotAnOwner() {
+        Post post = new Post();
+        post.setTitle("Foo");
+        post.setDescription("Foo");
+        post.setUsed(false);
+
+        postService.savePost(post);
+
         assertFalse(postService.isPostOwner(post.getId(), UUID.randomUUID()));
+    }
+
+    @Test
+    void postOwnerCheckThrowsExceptionIfNoPostExist() {
+        Post post = new Post();
+        post.setTitle("Foo");
+        post.setDescription("Foo");
+        post.setUsed(false);
+
+        postService.savePost(post);
+
         assertThrows(HttpStatusException.class, () -> postService.isPostOwner(UUID.randomUUID(), post.getAccountId()));
     }
 
     @Test
-    void updatePostById() {
-        Post expected = new Post();
-        expected.setTitle("Foo");
-        expected.setDescription("Foo");
-        expected.setPrice(200);
-        expected.setUsed(false);
+    void getPostByIdCheckThrowsExceptionIfNoPostExist() {
+        Post post = new Post();
+        post.setTitle("Foo");
+        post.setDescription("Foo");
+        post.setUsed(false);
 
-        postService.savePost(expected);
+        postService.savePost(post);
 
-        PostUpdateDTO updateDTO = new PostUpdateDTO(expected.getTitle(), expected.getDescription(), 100, true);
-        postMapper.updatePost(expected, updateDTO);
-        postService.updatePost(expected);
+        assertThrows(HttpStatusException.class, () -> postService.getPostById(UUID.randomUUID()));
+    }
 
-        Post actual = postService.getPostById(expected.getId());
-        expected.setUpdatedAt(actual.getUpdatedAt());
+    @Test
+    void getPostByIdCheckIsNotNullIfPostExist() {
+        Post post = new Post();
+        post.setTitle("Foo");
+        post.setDescription("Foo");
+        post.setUsed(false);
 
-        assertEquals(expected, actual);
+        postService.savePost(post);
+
+        assertNotNull(postService.getPostById(post.getId()));
+    }
+
+    @Test
+    void getPostByIdCheckIfSavedPostEqualsToFound() {
+        Post post = new Post();
+        post.setTitle("Foo");
+        post.setDescription("Foo");
+        post.setUsed(false);
+
+        postService.savePost(post);
+        Post found = postService.getPostById(post.getId());
+
+        assertEquals(post, found);
+    }
+
+    @Test
+    void getAllPostsCheckIsTrueIfNoPostsExist() {
+        assertTrue(postService.getAllPosts().isEmpty());
+    }
+
+    @Test
+    void getAllPostsCheckIsNotNullIfPostsExist() {
+        Post post = new Post();
+        post.setTitle("Foo");
+        post.setDescription("Foo");
+        post.setUsed(false);
+
+        postService.savePost(post);
+        List<Post> posts = postService.getAllPosts();
+
+        assertNotNull(posts);
+    }
+
+    @Test
+    void getAllPostsCheckIsTrueIfPostsFoundDeletedAtIsNull() {
+        Post post = new Post();
+        post.setTitle("Foo");
+        post.setDescription("Foo");
+        post.setUsed(false);
+
+        postService.savePost(post);
+        List<Post> posts = postService.getAllPosts();
+
+        assertTrue(posts.stream().allMatch(p -> p.getDeletedAt() == null));
+    }
+
+    @Test
+    void savePostCheckIfSavedPostEqualsToFound() {
+        Post post = new Post();
+        post.setTitle("Foo");
+        post.setDescription("Foo");
+        post.setUsed(false);
+
+        postService.savePost(post);
+
+        assertEquals(post, postService.getPostById(post.getId()));
     }
 }
